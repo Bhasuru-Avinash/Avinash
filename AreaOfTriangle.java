6ecf0b8106e4703bdad50a08af8b9e32cc06cb24
Avinash <aavi45538@gmail.com>
2022-07-21 21:08:16 +0530

    hello

---
+++ AreaOfTriangle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class AreaOfTriangle
+{
+  public static void main(String[] args)
+  {
+     Scanner sc=new Scanner(System.in);
+     System.out.print("Enter the base of triangle: ");
+     float e=sc.nextFloat();
+     System.out.print("Enter the height of triangle: ");
+     float f=sc.nextFloat();
+     float r=(e*f)/2;
+     System.out.print("Area of Triangle is: "+r+"\n");
+	
+		
+	}
+}
+
