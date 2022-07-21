c34992c7a3b744781f557d2d0bfa203d21a33bb0
Avinash <aavi45538@gmail.com>
2022-07-21 21:35:11 +0530

    hello

---
+++ PrimitiveTypes.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+class PrimitiveTypes
+{
+	public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+      System.out.print("Enter the byte value: ");
+      byte x=sc.nextByte();
+      System.out.print("Enter the short value: ");
+      short y=sc.nextShort();
+      System.out.print("Enter the int value: ");
+      int z=sc.nextInt();
+      System.out.print("Enter the long value: ");
+      long s=sc.nextLong();
+      System.out.print("Enter the boolean value: ");
+      boolean w=sc.nextBoolean();
+      System.out.print("Enter the double value: ");
+      double u=sc.nextDouble();
+      System.out.print("Enter the float value: ");
+      float v=sc.nextFloat();
+      System.out.println("byte default value = "+x);
+      System.out.println("short default value = "+y);
+      System.out.println("int default value = "+z);
+      System.out.println("long default value = "+s);
+      System.out.println("boolean default value = "+w);
+      System.out.println("double default value = "+u);
+      System.out.println("float default value = "+v);
+    }
+}
+      
+      
+      
+        
+
