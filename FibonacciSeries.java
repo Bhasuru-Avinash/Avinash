0098865c58fa728980b196e8e91de84f0ad34af4
Avinash <aavi45538@gmail.com>
2022-07-21 21:23:15 +0530

    ho

---
+++ FibonacciSeries.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+class FibonacciSeries
+{
+	public static void main(String[] args)
+	{
+		int n = Integer.parseInt(args[0]);
+		int a=0,b=1;
+		System.out.print(a+" "+b+" ");
+		int c;
+		for(int i=1;i<=n;i++)
+		{
+			
+			if(a+b<=n)
+			{
+				c=a+b;
+				System.out.print(c);
+				a=b;
+				b=c;
+			}
+			if(a+b<=n)
+			{
+				System.out.print(" ");
+			}
+		}
+		System.out.println("");
+	  }
+}
+
